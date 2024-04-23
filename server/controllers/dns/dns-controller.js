7731c7214a57157b0f60aeb786fe80da525055b7
frankhart2018 <sdhargithub@gmail.com>
2024-04-22 22:11:33 -0500

    Add api to ping urls

--- server/controllers/dns/dns-controller.js
+++ server/controllers/dns/dns-controller.js
@@ -1,4 +1,5 @@
-import { getCurrentTime } from "../../utils/date-utils.js";
+import { CommandExecutor, PipeExecuteStrategy } from "command-executor-lib";
+
 import { sendRespone } from "../../utils/response-utils.js";
 import {
   addARecord,
@@ -63,8 +64,6 @@ const getARecordsHandler = async (req, res, logger) => {
 };
 
 const deleteARecordHandler = async (req, res, logger) => {
-  console.log(req);
-
   const zoneName = req.params.zoneName;
   const aName = req.body.aName;
 
@@ -78,13 +77,32 @@ const deleteARecordHandler = async (req, res, logger) => {
   }
 };
 
+const pingUrlHandler = async (req, res, logger) => {
+  const { url } = req.params;
+
+  const executorStrategy = PipeExecuteStrategy.builder().withPipePath("command-runner").build();
+  const command = `dig +short ${url}`;
+  const commandExecutor = new CommandExecutor(command, executorStrategy);
+  const result = commandExecutor.execute();
+
+  if (result.type === 0) {
+    sendRespone(req, res, logger, "info", 200, {
+      resolved: result.value.trim().length > 0,
+    });
+  } else {
+    sendRespone(req, res, logger, "error", 500, {
+      error: result.value.trim(),
+    });
+  }
+};
+
 const DnsController = (app, logger) => {
   //////////////////////////////////////////
   // ZONE
   //////////////////////////////////////////
   app.put("/zone", (req, res) => createZoneHandler(req, res, logger));
   app.delete("/zone/:zoneName", (req, res) =>
-    deleteZoneHandler(req, res, logger),
+    deleteZoneHandler(req, res, logger)
   );
   app.get("/zones", (req, res) => getAllZonesHandler(req, res, logger));
 
@@ -92,12 +110,17 @@ const DnsController = (app, logger) => {
   // A RECORD
   //////////////////////////////////////////
   app.get("/a-records/:zoneName", (req, res) =>
-    getARecordsHandler(req, res, logger),
+    getARecordsHandler(req, res, logger)
   );
   app.post("/a-record", (req, res) => addARecordHandler(req, res, logger));
   app.delete("/a-record/:zoneName", (req, res) =>
-    deleteARecordHandler(req, res, logger),
+    deleteARecordHandler(req, res, logger)
   );
+
+  //////////////////////////////////////////
+  // TEST THINGS
+  //////////////////////////////////////////
+  app.get("/ping/:url", (req, res) => pingUrlHandler(req, res, logger));
 };
 
 export default DnsController;
