8e1baa0ab8efbdc4249159f5cf7cb62b806a769f
frankhart2018 <sdhargithub@gmail.com>
2024-04-23 18:03:28 -0500

    Add empty fields check while adding a-names

--- frontend/src/components/pages/ListANames/ListANames.js
+++ frontend/src/components/pages/ListANames/ListANames.js
@@ -86,6 +86,11 @@ const ListANames = () => {
   };
 
   const addANameHandler = () => {
+    if (aName === "" || ip === "") {
+      alert("Please fill in all fields");
+      return;
+    }
+
     const ipRegex = new RegExp(
       "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
     );
